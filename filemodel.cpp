456eb002180c8730472484fceb7aa90fcd61dc6b
Markus Heilig <heilig@bond-it.de>
2017-08-31 11:46:21 +0200

    save

--- filemodel.cpp
+++ filemodel.cpp
@@ -7,7 +7,6 @@ FileModel::FileModel()
   : QAbstractTableModel()
 {
     connect(&watcher, &QFileSystemWatcher::directoryChanged, this, &FileModel::onDirectoryChanged);
-    connect(&watcher, &QFileSystemWatcher::fileChanged, this, &FileModel::onFileChanged);
 }
 
 QVariant FileModel::headerData(int section, Qt::Orientation orientation, int role) const
@@ -23,10 +22,40 @@ QVariant FileModel::headerData(int section, Qt::Orientation orientation, int rol
     return QAbstractTableModel::headerData(section, orientation, role);
 }
 
+
+int FileModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return modelList.count();
+}
+
+int FileModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return columns_len;
+}
+
+QVariant FileModel::data(const QModelIndex &index, int role) const
+{
+    Q_UNUSED(role);
+
+    if (role == Qt::DisplayRole) {
+        const QFileInfo &file = modelList.at(index.row());
+        if (index.column() == filename) {
+            return file.absoluteFilePath().remove(0, dir.absolutePath().size() + 1);
+        }
+        if (index.column() == lastModified) {
+            return file.lastModified();
+        }
+    }
+    return QVariant();
+}
+
+
 QFileInfo FileModel::getFileAt(int row) const
 {
-    if (row >= 0 && row < files.count()) {
-        return files.at(row);
+    if (row >= 0 && row < modelList.count()) {
+        return modelList.at(row);
     }
     return QFileInfo();
 }
@@ -44,158 +73,171 @@ QDir FileModel::getDir() const
     return dir;
 }
 
-bool FileModel::contains(const QFileInfo &info) const
+QPair<QFileInfoList, QFileInfoList> FileModel::getDirectoriesAndFiles(const QDir &dir) const
 {
-    return getRow(info) != -1;
-}
-
-QDateTime FileModel::getLastModified(const QFileInfo &info)
-{
-    for (int i = 0; i < files.size(); i++) {
-        QFileInfo fileInfo = files.at(i);
-        if (fileInfo.absoluteFilePath() == info.absoluteFilePath()) {
-            return fileInfo.lastModified();
+    QFileInfoList dirs;
+    QFileInfoList files;
+    QDirIterator it(dir,  QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        QFileInfo fi = it.next();
+        if (fi.isFile()) {
+            files.append(fi);
+        } else if (fi.isDir()) {
+            dirs.append(fi);
         }
     }
-    return QDateTime();
+
+    // iterator also gives us the parent directory of 'dir'
+    // but we don't want it! so lets remove it:
+    QDir parent = dir;
+    if (parent.cdUp()) {
+        dirs.removeOne(QFileInfo(parent.absolutePath()));
+    }
+
+    return QPair<QFileInfoList, QFileInfoList>(dirs, files);
 }
 
-/*
-bool FileModel::removeFile(const QFileInfo &info)
+QFileInfoList FileModel::toFileInfoList(const QStringSet &filePaths) const
 {
-    int row = getRow(info);
-    bool exists = row != -1;
-    if (exists) {
-        beginResetModel();
-        files.removeAt(row);
-        endResetModel();
+    QFileInfoList list;
+    foreach (const QString &filePath, filePaths) {
+        list.append(filePath);
     }
-    return exists;
+    return list;
 }
-*/
+
+QFileInfoList FileModel::sortByLastModified(const QFileInfoList &files) const
+{
+    QMultiMap<QDateTime, QFileInfo> map;
+    foreach (const QFileInfo &fi, files) {
+        map.insertMulti(fi.lastModified(), fi);
+    }
+    return map.values();
+}
+
+QStringSet FileModel::getAbsolutePaths(const QFileInfoList &paths) const
+{
+    QStringSet absolutePaths;
+    foreach (const QFileInfo &fi, paths) {
+        absolutePaths.insert(fi.absoluteFilePath());
+    }
+    return absolutePaths;
+}
+
+QStringSet FileModel::checkUpdatedFiles(const QStringSet &filesToCheck) const
+{
+    QStringSet updatedFiles;
+    foreach (const QString &file, filesToCheck) {
+        QFileInfo fi = file;
+        if (fi.lastModified() != getLastKnownModifiedDate(fi)) {
+            updatedFiles.insert(file);
+        }
+    }
+    return updatedFiles;
+}
+
+QDateTime FileModel::getLastKnownModifiedDate(const QFileInfo &info) const
+{
+    return getFileInfoByPath(info.absoluteFilePath()).lastModified();
+}
+
+QStringList FileModel::getRelativeFilePaths(const QStringList &absolutePaths) const
+{
+    QStringList relativePaths;
+    foreach (const QString &absolutePath, absolutePaths) {
+        relativePaths << dir.relativeFilePath(absolutePath);
+    }
+    return relativePaths;
+}
+
+QFileInfo FileModel::getFileInfoByPath(const QString &absoluteFilePath) const
+{
+    QListIterator<QFileInfo> iter(modelList);
+    while (iter.hasNext()) {
+        QFileInfo fi = iter.next();
+        if (fi.absoluteFilePath() == absoluteFilePath) {
+            return fi;
+        }
+    }
+    return QFileInfo();
+}
+
+
 void FileModel::onDirectoryChanged(const QString &path)
 {
-    // wird das auch bei subdirectories aufgerufen?
-    // wenn eine datei im subdirectory erstellt wird?
+    Q_UNUSED(path);
 
-    QMultiMap<QDateTime, QFileInfo> map;
-    QDirIterator iterator(QDir(path), QDirIterator::Subdirectories);
-    while (iterator.hasNext()) {
-        const QFileInfo &fi = iterator.next();
-        if (fi.isFile()) {
-            int row = getRow(fi);
-            if (row == -1) {
-                // new file
-                watcher.addPath(fi.absoluteFilePath());
-                map.insertMulti(fi.lastModified(), fi);
-            } else if (fi.lastModified() != getLastModified(fi)) {
-                // updated file
-                files.removeAt(row);
-                map.insertMulti(fi.lastModified(), fi);
-            }
-        }
+    emit beginResetModel();
+
+    QPair<QFileInfoList, QFileInfoList> tuple = getDirectoriesAndFiles(dir);
+    QStringSet directories = getAbsolutePaths(tuple.first);
+    QStringSet files = getAbsolutePaths(tuple.second);
+    QStringSet newDirectories = QStringSet(directories).subtract(knownDirectories);
+    QStringSet newFiles = QStringSet(files).subtract(knownFiles);
+    QStringSet removedDirectories = QStringSet(knownDirectories).subtract(directories);
+    QStringSet removedFiles = QStringSet(knownFiles).subtract(files);
+    QStringSet existingFiles = QStringSet(knownFiles).intersect(files);
+    QStringSet updatedFiles = checkUpdatedFiles(existingFiles);
+
+    // update internal state
+
+    QStringSet toRemove = removedFiles.unite(updatedFiles);
+    foreach (const QFileInfo &file, toRemove) {
+        modelList.removeAll(file);
     }
 
-    if (!map.isEmpty()) {
-        emit beginResetModel();
-        QListIterator<QFileInfo> iter(map.values());
-        while (iter.hasNext()) {
-            files.prepend(iter.next());
-        }
-        emit endResetModel();
+    QFileInfoList toInsert = toFileInfoList(QStringSet(newFiles).unite(updatedFiles));
+    QFileInfoList sorted = sortByLastModified(toInsert);
+    foreach (const QFileInfo &fileInfo, sorted) {
+        modelList.prepend(fileInfo);
     }
-}
 
-void FileModel::onFileChanged(const QString &path)
-{
-    QFileInfo fileInfo(path);
-    int row = getRow(fileInfo);
-    bool changedFile = false;
-    beginResetModel();
-    if (row != -1) {
-        files.removeAt(row);
-    }
-    if (fileInfo.exists()) {
-        changedFile = true;
-        files.prepend(fileInfo);
-    }
-    endResetModel();
+    knownDirectories = directories;
+    knownFiles = files;
 
-    if (changedFile) {
-        // todo: emit signal 'changed file'
+    // add new files and directories to watcher
+    QStringList watch = QStringSet(newDirectories).unite(newFiles).toList();
+    if (!watch.isEmpty()) {
+        watcher.addPaths(watch);
     }
+
+    // discard removed files and directories from watcher
+    QStringList unwatch = QStringSet(removedDirectories).unite(removedFiles).toList();
+    if (!unwatch.isEmpty()) {
+        watcher.removePaths(unwatch);
+    }
+
+    // send signals
+    if (!newFiles.isEmpty() || !updatedFiles.isEmpty()) {
+        emit fileUpdate(getRelativeFilePaths(newFiles.toList()),
+                        getRelativeFilePaths(updatedFiles.toList()));
+    }
+
+    emit endResetModel();
 }
 
 void FileModel::loadDirectoryAsync()
 {
     beginResetModel();
 
-    // remove watcher
-    QStringList paths;
-    paths << watcher.directories() << watcher.files();
-    if (!paths.isEmpty()) {
-        watcher.removePaths(paths);
+    QStringList unwatch = knownDirectories.unite(knownDirectories).toList();
+    if (!unwatch.isEmpty()) {
+        watcher.removePaths(unwatch);
     }
 
-    // load files
-    QMultiMap<QDateTime, QFileInfo> map;
-    QDirIterator iterator(dir, QDirIterator::Subdirectories);
-    while (iterator.hasNext()) {
-        const QFileInfo &fi = iterator.next();
-        if (fi.isFile()) {
-            map.insertMulti(fi.lastModified(), fi);
-        }
-        watcher.addPath(fi.absoluteFilePath());
-    }
+    QPair<QFileInfoList, QFileInfoList> tuple = getDirectoriesAndFiles(dir);
+    QFileInfoList dirs = tuple.first;
+    QFileInfoList files = tuple.second;
 
-    // sort
-    QFileInfoList sortedFiles;
-    QListIterator<QFileInfo> iter(map.values());
-    iter.toBack();
-    while (iter.hasPrevious()) {
-        QFileInfo fileInfo = iter.previous();
-        sortedFiles.append(fileInfo);
+    modelList = sortByLastModified(files);
+    std::reverse(modelList.begin(), modelList.end());
+    knownDirectories = getAbsolutePaths(dirs);
+    knownFiles = getAbsolutePaths(files);
+
+    QStringList watch = knownDirectories.unite(knownDirectories).toList();
+    if (!watch.isEmpty()) {
+        watcher.addPaths(watch);
     }
-    this->files = sortedFiles;
 
     endResetModel();
-}
-
-int FileModel::getRow(const QFileInfo &info) const
-{
-    for (int i = 0; i < files.size(); i++) {
-        QFileInfo fileInfo = files.at(i);
-        if (fileInfo.absoluteFilePath() == info.absoluteFilePath()) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int FileModel::rowCount(const QModelIndex &parent) const
-{
-    Q_UNUSED(parent);
-    return files.count();
-}
-
-int FileModel::columnCount(const QModelIndex &parent) const
-{
-    Q_UNUSED(parent);
-    return columns_len;
-}
-
-QVariant FileModel::data(const QModelIndex &index, int role) const
-{
-    Q_UNUSED(role);
-
-    if (role == Qt::DisplayRole) {
-        const QFileInfo &file = files.at(index.row());
-        if (index.column() == filename) {            
-            return file.absoluteFilePath().remove(0, dir.absolutePath().size() + 1);
-        }
-        if (index.column() == lastModified) {
-            return file.lastModified();
-        }
-    }
-    return QVariant();
 }
